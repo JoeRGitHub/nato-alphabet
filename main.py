702573e016fb177975e1efa892980a7f75672664
JoeRGitHub <36372160+JoeRGitHub@users.noreply.github.com>
2024-10-22 18:10:31 +0300

    Add check for non-alphabet input

--- main.py
+++ main.py
@@ -4,25 +4,26 @@ import pandas
 # TODO 1. create a dict from the CSV
 df = pandas.read_csv('nato_phonetic_alphabet.csv')
 index_phonetic_alphabet = df.set_index('letter')['code'].to_dict()
-name = input("Enter the name you need in your alphabet: ")
-#name ='didiy'
 
-<<<<<<< Updated upstream
-# def word(user_input):
-#     name = input("Enter the name you need in your alphabet: ")
-=======
-def word(   ):
+
+# TODO 2. check user types only alphabet
+def word():
     name = input("Enter the name you need in your alphabet: ")
->>>>>>> Stashed changes
+    if name.isalpha():
+        return name
+    else:
+        print("Use only alphabet and not space. Try again")
+        return word()
 
-# TODO 2. The user writes text and each letter is translated into a phonetic alphabet:
+
+# TODO 3. The user writes text and each letter is translated into a phonetic alphabet:
 new_phonetic_name = []
-for letter in name.upper():
+x = word()
+for letter in x.upper():
     for code_letter, code_names in index_phonetic_alphabet.items():
         if letter == code_letter:
             new_phonetic_name.append(code_names)
-        else:
-            "Use only letters, Try again"
+
 
 print(new_phonetic_name)
 
