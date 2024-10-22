e85f19b6e9ebe42548b48f9b39875f6143b0398d
JoeRGitHub <36372160+JoeRGitHub@users.noreply.github.com>
2024-10-22 21:55:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import pandas
 # TODO 1. create a dict from the CSV
 df = pandas.read_csv('nato_phonetic_alphabet.csv')
 index_phonetic_alphabet = df.set_index('letter')['code'].to_dict()
-<<<<<<< HEAD
 
 
 # TODO 2. check user types only alphabet
@@ -25,22 +24,6 @@ for letter in x.upper():
         if letter == code_letter:
             new_phonetic_name.append(code_names)
 
-=======
-name = input("Enter the name you need in your alphabet: ")
-#name ='didiy'
-
-# def word(user_input):
-#     name = input("Enter the name you need in your alphabet: ")
-
-# TODO 2. The user writes text and each letter is translated into a phonetic alphabet:
-new_phonetic_name = []
-for letter in name.upper():
-    for code_letter, code_names in index_phonetic_alphabet.items():
-        if letter == code_letter:
-            new_phonetic_name.append(code_names)
-        else:
-            "Use only letters, Try again"
->>>>>>> add-alphabet-check
 
 print(new_phonetic_name)
 
