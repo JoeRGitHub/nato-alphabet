56ac26aefdedcd73a8f9c0e4733dec8b73a536e4
Yossi Rozen <yossi.rozen@morphisec.com>
2024-10-22 13:56:53 +0300

    clen code

--- main.py
+++ main.py
@@ -1,41 +1,24 @@
-# TODO 1. create a dict in this format:
-# {"A": "Alfa", "B", Bravo}
-
-# TODO 2. The user writes text and each letter is translated into a phonetic alphabet:
-# Thomas
-# ['Tango', 'Hotel', 'Oscar']
-
-# Working with pands
 import pandas
 
-# step one: create a dict from the CSV
-df = pandas.read_csv('nato_phonetic_alphabet.csv')
-#print(df)
-index_phonetic_alphabet = df.set_index('letter')['code'].to_dict()
-#print(index_phonetic_alphabet)
 
-# index_phonetic_alphabet_com = {key: value for (key, value) in df.items()}
-# print(index_phonetic_alphabet_com)
+# TODO 1. create a dict from the CSV
+df = pandas.read_csv('nato_phonetic_alphabet.csv')
+index_phonetic_alphabet = df.set_index('letter')['code'].to_dict()
 name = input("Enter the name you need in your alphabet: ")
 #name ='didiy'
 
-new_phonetic_name = []
+# def word(user_input):
+#     name = input("Enter the name you need in your alphabet: ")
 
-# step two: create a loop that will run on the dict
+# TODO 2. The user writes text and each letter is translated into a phonetic alphabet:
+new_phonetic_name = []
 for letter in name.upper():
     for code_letter, code_names in index_phonetic_alphabet.items():
         if letter == code_letter:
             new_phonetic_name.append(code_names)
-print('For loop:', new_phonetic_name)
-#
-# name_letters = [code_names for letter in name.upper() for (code_letter, code_names) in index_phonetic_alphabet.items()
-#                 if letter == code_letter]
-# print('List Comprehension:', name_letters)
+        else:
+            "Use only letters, Try again"
 
-# Try to create the dictionary comprehension without using the 'index_phonetic_alphabet'
-
-
-
-
+print(new_phonetic_name)
 
 
